5c0dbe2d81d5229b8a44c70886bd7aa04f85894b
Ram <ramonrails@gmail.com>
2020-07-20 16:32:36 +0530

    supress console output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,4 +18,19 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  # supress STDOUT console output (API calls)
+  original_stderr = $stderr
+  original_stdout = $stdout
+
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, 'w')
+    $stdout = File.open(File::NULL, 'w')
+  end
+
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
 end
